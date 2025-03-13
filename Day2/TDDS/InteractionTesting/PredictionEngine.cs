b686f38a1e2849e3094b910126f6aa420c8de3b6
Ankur Hirwani <73126292+ankur-hirwani@users.noreply.github.com>
2025-03-13 17:18:45 +0530

    Add files via upload

---
+++ Day2/TDDS/InteractionTesting/PredictionEngine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PredictionEngine
+{
+    public class PredictionEngine
+    {
+        private ILanguageModelAlgo languageModelAlgo;
+
+        public PredictionEngine(ILanguageModelAlgo languageModelAlgo)
+        {
+            this.languageModelAlgo = languageModelAlgo;
+        }
+
+        public string Predict(string inputPhrase)
+        {
+            string lastWord = GetLastWord(inputPhrase);
+
+            if(inputPhrase.EndsWith(" "))
+                return languageModelAlgo.predictUsingBigram(lastWord);
+            
+            return languageModelAlgo.predictUsingMonogram(lastWord);
+        }
+
+        private string GetLastWord(string input)
+        {
+            string lastWord = input.Trim().Split(" ").Last();
+            return lastWord;
+        }
+
+    }
+}
